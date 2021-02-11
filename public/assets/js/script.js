e5e2b98c83bf2a295142d4eff9e5f1b0b588ae1d
dvicjon <devinvjones@gmail.com>
2021-02-11 13:11:24 -0500

    11.3.5

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -25,7 +25,23 @@ const handleAnimalFormSubmit = event => {
     personalityTraits.push(selectedTraits[i].value);
   }
   const animalObject = { name, species, diet, personalityTraits };
-
+  //11.3.5 
+  fetch('./api/animals', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalObject)
+  }).then (response => {
+    if (response.ok) {
+      return response.json(); 
+    }
+    alert ('Error: ' + response.statusText);
+  }).then (postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding an animal!');
+  });
 };
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
