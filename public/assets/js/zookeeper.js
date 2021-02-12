1f5e857b902d78872030ae1e1a9d40984947d02c
dvicjon <devinvjones@gmail.com>
2021-02-12 13:16:38 -0500

    11.4.7

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,5 +1,5 @@
 const $displayArea = document.querySelector('#display-area');
-
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 const printResults = resultArr => {
   console.log(resultArr);
 
@@ -21,7 +21,13 @@ const printResults = resultArr => {
 };
 
 const getZookeepers = () => {
-  fetch('/api/zookeepers')
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key,value]) => {
+    queryUrl += `${key}=${value}&`; 
+  });
+
+  fetch(queryUrl)
     .then(response => {
       if (!response.ok) {
         return alert('Error: ' + response.statusText);
@@ -34,4 +40,18 @@ const getZookeepers = () => {
     });
 };
 
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObject = { name, age };
+
+  getZookeepers(zookeeperObject);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 getZookeepers();
